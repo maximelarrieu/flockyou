314b324108989951884f4df4512317d35dccc15e
Maxime Larrieu <maximelarrieu@github.com>
2019-12-11 16:59:01 +0100

    :wrench: lib

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'leagues',
+    loadChildren: () => import('./leagues/leagues.module').then( m => m.LeaguesPageModule)
+  },
 ];
 
 @NgModule({
--- application/src/app/base/base.page.spec.ts
+++ application/src/app/base/base.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab1Page } from './tab1.page';
+import { BasePage } from './base.page';
 
-describe('Tab1Page', () => {
-  let component: Tab1Page;
-  let fixture: ComponentFixture<Tab1Page>;
+describe('BasePage', () => {
+  let component: BasePage;
+  let fixture: ComponentFixture<BasePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab1Page],
+      declarations: [ BasePage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab1Page);
+    fixture = TestBed.createComponent(BasePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
