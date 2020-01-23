b908c5a3ad6056d90032f44da25fa2b89dd43da7
Maxime Larrieu <maximelarrieu@github.com>
2020-01-23 10:41:56 +0100

    :sparkles: function to route product

--- website/src/Controller/HomeController.php
+++ website/src/Controller/HomeController.php
@@ -2,27 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
+use App\Repository\LeaguesRepository;
+
+use App\Repository\SizesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @var ProductsRepository
      */
+    private $productsRepository;
+
+    /**
+     * @var LeaguesRepository
+     */
+    private $leaguesRepository;
+
+    /**
+     * @var SizesRepository
+     */
+    private $sizesRepository;
+
+    /**
+     * HomeController constructor.
+     *
+     * @param LeaguesRepository $leaguesRepository
+     * @param ProductsRepository $productsRepository
+     * @param SizesRepository $sizesRepository
+     */
+    public function __construct(LeaguesRepository $leaguesRepository, ProductsRepository $productsRepository, SizesRepository $sizesRepository)
+    {
+        $this->productsRepository =  $productsRepository;
+        $this->leaguesRepository = $leaguesRepository;
+        $this->sizesRepository = $sizesRepository;
+    }
+
     public function index()
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $this->productsRepository->findAll(),
+            'sizes' => $this->sizesRepository->findAll()
         ]);
     }
 
     public function navbar() {
-        return $this->render('header.html.twig');
+        return $this->render('header.html.twig', [
+            'leagues' => $this->leaguesRepository->findAll()
+        ]);
     }
 
     public function caroussel() {
-        return $this->render('home/caroussel.html.twig');
+        return $this->render('caroussel.html.twig');
     }
 
     public function footer() {
