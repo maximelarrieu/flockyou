ef65616f4a59e0dd7fb10eb9519eb9b75cbabc09
Maxime Larrieu <maximelarrieu@github.com>
2020-01-29 18:05:04 +0100

    :sparkles: entity user & inscription form

---
+++ website/src/Form/AccountType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
---
+++ website/src/Form/PasswordEditType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordEditType extends AbstractType
+{
+    /**
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = []) {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class, $this->getConfiguration("Mot de passe actuel", "Renseignez votre mot de passe actuel..."))
+            ->add('newPassword', PasswordType::class, $this->getConfiguration("Nouveau mot de passe", "Renseignez votre nouveau mot de passe..."))
+            ->add('confirmPassword', PasswordType::class, $this->getConfiguration("Confirmez le nouveau mot de passe", "Confirmez votre nouveau mot de passe..."))
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
