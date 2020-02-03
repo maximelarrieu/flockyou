3db4cdeb04bab16eb3185e5d94733bcea93f4e38
Maxime Larrieu <maximelarrieu@github.com>
2020-02-03 17:59:50 +0100

    :sparkles: product form

---
+++ website/src/Form/ProductType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Products;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('product_img')
+            ->add('price', MoneyType::class)
+//            ->add('team')
+//            ->add('state')
+//            ->add('stock')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
