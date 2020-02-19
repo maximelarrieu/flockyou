ea53d443fa79859dba3155a4a30290e614580541
Maxime Larrieu <maximelarrieu@github.com>
2020-02-19 18:55:32 +0100

    :sparkles: form to edit products in admin

--- website/src/Form/ProductType.php
+++ website/src/Form/ProductType.php
@@ -16,7 +16,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType extends AbstractType
+class ProductType extends ConfigType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
