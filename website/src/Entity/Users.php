89a3b5e7cc2742921c58041825dd7ae8e9eee268
maximelarrieu <maxime.larrieu@ynov.com>
2020-03-22 15:29:38 +0100

    :wrench: add to api platform

--- website/src/Entity/Users.php
+++ website/src/Entity/Users.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -10,6 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
  * @UniqueEntity(
  *     fields={"email"},
