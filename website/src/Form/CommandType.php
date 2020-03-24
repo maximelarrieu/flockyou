9b1ae06f619ffbc7194c7b3c913bd3abbc83e6d9
maximelarrieu <maxime.larrieu@ynov.com>
2020-03-24 19:26:39 +0100

    :sparkles: command form

---
+++ website/src/Form/CommandType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Command;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+//            ->add('quantity')
+//            ->add('nb_command')
+//            ->add('cart')
+//            ->add('livraison')
+//            ->add('bank')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+}
