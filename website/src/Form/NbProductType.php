6363ffc8436b6e2287648854640873d554b19e73
maximelarrieu <maxime.larrieu@ynov.com>
2020-03-30 21:32:42 +0200

    :wrench: test

---
+++ website/src/Form/NbProductType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CartProduct;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NbProductType extends ConfigType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('quantity', IntegerType::class, $this->getConfiguration('Quantité souahitée :', 'Indiquez la quantité voulue', [
+                'attr' => [
+                    'min' => 1,
+                    'max' => 10,
+                    'step' => 1
+                ]
+            ]))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CartProduct::class,
+        ]);
+    }
+}
