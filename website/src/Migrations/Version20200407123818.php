249b61d0664c06c96f060ac4fa2a085f636fa9b0
maximelarrieu <maxime.larrieu@ynov.com>
2020-04-07 20:20:51 +0200

    :wrench: good version from migration

--- website/src/Migrations/Version20200407123818.php
+++ website/src/Migrations/Version20200407123818.php
@@ -22,10 +22,10 @@ final class Version20200407123818 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE commands (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, nb_command INT DEFAULT NULL, total FLOAT DEFAULT NULL, created_at DATETIME DEFAULT NULL, INDEX IDX_8ECAEAD4A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE command (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, nb_command INT DEFAULT NULL, total FLOAT DEFAULT NULL, created_at DATETIME DEFAULT NULL, INDEX IDX_8ECAEAD4A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE command_cart_product (command_id INT NOT NULL, cart_product_id INT NOT NULL, INDEX IDX_2FA703C33E1689A (command_id), INDEX IDX_2FA703C25EE16A8 (cart_product_id), PRIMARY KEY(command_id, cart_product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE commands ADD CONSTRAINT FK_8ECAEAD4A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE command_cart_product ADD CONSTRAINT FK_2FA703C33E1689A FOREIGN KEY (command_id) REFERENCES commands (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE command ADD CONSTRAINT FK_8ECAEAD4A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE command_cart_product ADD CONSTRAINT FK_2FA703C33E1689A FOREIGN KEY (command_id) REFERENCES command (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE command_cart_product ADD CONSTRAINT FK_2FA703C25EE16A8 FOREIGN KEY (cart_product_id) REFERENCES cart_product (id) ON DELETE CASCADE');
     }
 
@@ -35,7 +35,7 @@ final class Version20200407123818 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE command_cart_product DROP FOREIGN KEY FK_2FA703C33E1689A');
-        $this->addSql('DROP TABLE commands');
+        $this->addSql('DROP TABLE command');
         $this->addSql('DROP TABLE command_cart_product');
     }
 }
