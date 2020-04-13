d348f8597deddc29d555aa4220e947d20420fc8e
maximelarrieu <maxime.larrieu@ynov.com>
2020-04-13 21:56:13 +0200

    :sparkles: call repository function to display commands in desc order

--- website/src/Controller/CommandController.php
+++ website/src/Controller/CommandController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Command;
+use App\Repository\CommandRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommandController extends AbstractController
 {
+    /**
+     * @var CommandRepository
+     */
+    private $commandRepository;
+
+    public function __construct(CommandRepository $commandRepository) {
+        $this->commandRepository = $commandRepository;
+    }
+
     public function index()
     {
         $user = $this->getUser();
-
         return $this->render('command/index.html.twig', [
+            'commands' => $this->commandRepository->getUserCommands($user),
             'user' => $user,
         ]);
     }
